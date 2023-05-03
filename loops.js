2b5625a588916d49c4e1a9baee0dd6caf9c9dfb8
Jacob-Moore_lmig <Jacob.Moore@LibertyMutual.com>
2023-05-03 09:45:08 -0400

    adding loops

---
+++ loops.js
@@ -0,0 +1,19 @@
+//Loops template
+// Write a for loop that counts from 0 to 100, printing each number to the console.
+
+// Write a for loop that counts from 0 to 100 in increments of 2, printing each number to the console.
+
+// Write a for loop that counts down from 100 to 0, printing each number to the console.
+
+// Write a for loop that counts from 0 to 50 but only prints the odd numbers that are multiples of 5 to the console.
+
+
+// for(let i=0;i<101;i++){
+    
+//     console.log('Number'+ [i])
+// }
+
+for(let i=100;i>=0;i-=1){
+    
+    console.log('Number'+ [i])
+}
