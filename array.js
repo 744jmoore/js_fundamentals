8807b725a3b8c4f79387dc911af4004fcfafd80d
Jacob-Moore_lmig <Jacob.Moore@LibertyMutual.com>
2023-05-10 20:13:20 -0400

    testing find function

--- array.js
+++ array.js
@@ -1,14 +1,14 @@
 //Print "apple" from each of the arrays below to the console
 
 const fruits1 = ["apple", "pear", "strawberry", "grape"];
-console.log(fruits1[0])
+// console.log(fruits1[0])
 
 const fruits2 = [
     ["strawberry", "grape"],
     ["rambutan", "durian"],
     ["orange", "apple"]
 ];
-console.log(fruits2[2][1])
+// console.log(fruits2[2][1])
 
 const fruits3 = [
     ["strawberry",
@@ -20,4 +20,9 @@ const fruits3 = [
     ]
 ];
 
-console.log(fruits3[1][1][1][0])
+// console.log(fruits3[1][1][1][0])
+
+const found = fruits1.find(function(item){
+    return item === 'apple';
+})
+console.log(found);
